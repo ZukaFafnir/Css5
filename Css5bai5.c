5ba2374e11f5874a6ebac05e5dd726c2ef8f0562
Nguyen Van Minh Tri <mtanime020705@gmail.com>
2024-12-18 01:35:42 +0700

    Add files via upload

---
+++ Css5bai5.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){	
+	for(int i=1;i<=10;i++){
+		printf("The times %d table:\n",i);
+		for(int j=1;j<=10;j++){
+		int kq=0;
+				printf("%dx%d=%d\n",i,j,i*j);			
+		}		
+	}
+	return 0;
+}
+
+	
